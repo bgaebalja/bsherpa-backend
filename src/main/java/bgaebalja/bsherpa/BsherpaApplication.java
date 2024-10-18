a40b72becad476e01aebbf121a1c56d1927fee95
hellmir <hellmir610@gmail.com>
2024-10-18 12:52:40 +0900

    Initial commit (#1)
    - 빌드 설정
        - gradle 설정 추가
    
    - 실행 환경 설정
        - production application 설정 추가
        - production application 클래스 추가
        - test application 설정 추가
    
    - 형상 관리
        - .gitignore 추가

---
+++ src/main/java/bgaebalja/bsherpa/BsherpaApplication.java
@@ -0,0 +1,11 @@
+package bgaebalja.bsherpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsherpaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BsherpaApplication.class, args);
+    }
+}
