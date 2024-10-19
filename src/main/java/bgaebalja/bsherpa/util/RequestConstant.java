8013322c3b10e7f1ae55460af20aaa5147a58c55
Sung Hyobin <hellmir610@gmail.com>
2024-10-19 17:55:11 +0900

    fix(swagger): 누락된 RequestConstant 클래스 추가 (#24)

---
+++ src/main/java/bgaebalja/bsherpa/util/RequestConstant.java
@@ -0,0 +1,7 @@
+package bgaebalja.bsherpa.util;
+
+public class RequestConstant {
+    public static final String JWT = "JWT";
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String HEADER = "header";
+}
