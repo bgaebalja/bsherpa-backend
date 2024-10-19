e5b07f6abf60187c4abd6b77d37c90f748d73d27
Sung Hyobin <hellmir610@gmail.com>
2024-10-19 16:44:57 +0900

    chore(audit): 엔티티의 일반적인 컬럼을 자동으로 관리하는 BaseEntity와 BaseGeneralEntity 클래스 추가 (#14)
    - AuditConfig 클래스
    
    - BaseEntity 클래스
        - CreatedAt 속성
        - ModifiedAt 속성
    
    - BaseGeneralEntity 클래스
        - ID 속성
        - deleteYn 속성

---
+++ src/main/java/bgaebalja/bsherpa/configuration/AuditConfig.java
@@ -0,0 +1,9 @@
+package bgaebalja.bsherpa.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditConfig {
+}
