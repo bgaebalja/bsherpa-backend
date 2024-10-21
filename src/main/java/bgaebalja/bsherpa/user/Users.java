9e17235de6081e172105c0a62b1fd6766ffc75eb
heosangbeom <luv_3_u@naver.com>
2024-10-21 19:06:47 +0900

    feat(user): 회원 도메인 추가
    - 회원
      - domain

---
+++ src/main/java/bgaebalja/bsherpa/user/Users.java
@@ -0,0 +1,38 @@
+package bgaebalja.bsherpa.user;
+
+import static lombok.AccessLevel.PROTECTED;
+
+import bgaebalja.bsherpa.audit.BaseGeneralEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
+public class Users extends BaseGeneralEntity {
+
+  @Id @GeneratedValue
+  private Long id;
+
+  @Column(nullable = false)
+  private String username;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(nullable = false)
+  private String userId;
+
+  public static Users createUser(String username, String password, String userId) {
+    return Users.builder()
+        .username(username)
+        .password(password)
+        .userId(userId).build();
+  }
+}
