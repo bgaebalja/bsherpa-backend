a46dd2259625e61ac764985ee3086a28ea11a7a8
hellmir <hellmir610@gmail.com>
2024-10-22 17:33:22 +0900

    fix(user): 삭제된 회원 관련 클래스 복구 (#69)
    - controller
    - domain
    - service

--- src/main/java/bgaebalja/bsherpa/user/repository/UserRepository.java
+++ src/main/java/bgaebalja/bsherpa/user/repository/UserRepository.java
@@ -2,9 +2,14 @@ package bgaebalja.bsherpa.user.repository;
 
 import bgaebalja.bsherpa.user.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
 
   Users findByEmail(String email);
+
+  @Query("select u from Users u join fetch u.roles r where u.email=:email")
+  Users getUserWithRoles(@Param("email") String email);
 
 }
