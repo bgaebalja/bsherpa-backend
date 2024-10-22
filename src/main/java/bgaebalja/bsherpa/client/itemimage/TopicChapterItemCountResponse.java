e84b640c2c017822b612301ee9f4123e0056e407
Sung Hyobin <hellmir610@gmail.com>
2024-10-22 22:11:55 +0900

    feat(question): T셀파 API로부터 소단원과 토픽단원 별 문제 개수 목록을 받아 오는 기능 추가 (#75)
    - 기능
        - 문제 개수 리스트 요청 양식을 통한 문제 개수 목록 요청
        - 요청 객체를 직렬화해 T셀파 API에 요청
        - API 응답을 Java 객체로 역직렬화
        - 200 status code 응답
    
    - 설정
        - application.yml에 T셀파 item count 요청 URL 환경변수 추가
    
    - 배포
        - Jenkinsfile에 T셀파 item count 요청 URL 환경변수 추가
        - docker-compose.yml에 T셀파 item count 요청 URL 환경변수 추가

---
+++ src/main/java/bgaebalja/bsherpa/client/itemimage/TopicChapterItemCountResponse.java
@@ -0,0 +1,9 @@
+package bgaebalja.bsherpa.client.itemimage;
+
+import lombok.Getter;
+
+@Getter
+public class TopicChapterItemCountResponse {
+    private long topicChapterId;
+    private int itemCount;
+}
