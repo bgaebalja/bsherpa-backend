26db69771d6bc7340c49cf7ddd740c5635bb2a9d
hellmir <hellmir610@gmail.com>
2024-10-23 10:39:43 +0900

    feat(book): T셀파 API로부터 교재의 시험지 리스트를 받아 오는 기능 추가 (#90)
    - 기능
        - 교재 ID를 통한 교재의 시험지 리스트 요청
        - 요청 객체를 직렬화해 T셀파 API에 요청
        - API 응답을 Java 객체로 역직렬화
        - 200 status code 응답
    
    - 설정
        - application.yml에 T셀파 book exams 요청 URL 환경변수 추가
    
    - 배포
        - Jenkinsfile에 T셀파 book exams 요청 URL 환경변수 추가
        - docker-compose.yml에 T셀파 book exams 요청 URL 환경변수 추가

--- src/main/java/bgaebalja/bsherpa/book/controller/BookExternalController.java
+++ src/main/java/bgaebalja/bsherpa/book/controller/BookExternalController.java
@@ -1,9 +1,6 @@
 package bgaebalja.bsherpa.book.controller;
 
-import bgaebalja.bsherpa.client.chapter.ChapterApiClient;
-import bgaebalja.bsherpa.client.chapter.GetBookResponse;
-import bgaebalja.bsherpa.client.chapter.GetChaptersResponse;
-import bgaebalja.bsherpa.client.chapter.GetEvaluationsResponse;
+import bgaebalja.bsherpa.client.chapter.*;
 import bgaebalja.bsherpa.util.FormatValidator;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -34,13 +31,15 @@ public class BookExternalController {
     private static final String GET_SUBJECT_EVALUATIONS_FROM_TSHERPA_DESCRIPTION
             = "교재 ID를 입력해 T셀파의 교재 별 평가 영역 목록을 조회할 수 있습니다.";
 
+    private static final String GET_BOOK_EXAMS_FROM_TSHERPA = "T셀파의 교재 별 시험지 목록 조회";
+    private static final String GET_BOOK_EXAMS_FROM_TSHERPA_DESCRIPTION
+            = "교재 ID를 입력해 T셀파의 교재 별 시험지 목록을 조회할 수 있습니다.";
+
     private static final String TSHERPA_SUBJECT_ID = "T셀파의 교재 ID";
     private static final String TSHERPA_SUBJECT_ID_EXAMPLE = "1136";
 
     @GetMapping()
-    @ApiOperation(
-            value = GET_SUBJECT_FROM_TSHERPA, notes = GET_SUBJECT_FROM_TSHERPA_DESCRIPTION
-    )
+    @ApiOperation(value = GET_SUBJECT_FROM_TSHERPA, notes = GET_SUBJECT_FROM_TSHERPA_DESCRIPTION)
     public ResponseEntity<GetBookResponse> getSubjectFromTsherpa(
             @ApiParam(value = TSHERPA_SUBJECT_ID, example = TSHERPA_SUBJECT_ID_EXAMPLE)
             @RequestParam String subjectId
@@ -72,5 +71,16 @@ public class BookExternalController {
         FormatValidator.validatePositiveInteger(subjectId);
 
         return ResponseEntity.status(OK).body(chapterApiClient.getEvaluations(subjectId));
+    }
+
+    @GetMapping("/exams")
+    @ApiOperation(value = GET_BOOK_EXAMS_FROM_TSHERPA, notes = GET_BOOK_EXAMS_FROM_TSHERPA_DESCRIPTION)
+    public ResponseEntity<GetExamsResponse> getSubjectExamsFromTsherpa(
+            @ApiParam(value = TSHERPA_SUBJECT_ID, example = TSHERPA_SUBJECT_ID_EXAMPLE)
+            @RequestParam String subjectId
+    ) {
+        FormatValidator.validatePositiveInteger(subjectId);
+
+        return ResponseEntity.status(OK).body(chapterApiClient.getExams(subjectId));
     }
 }
