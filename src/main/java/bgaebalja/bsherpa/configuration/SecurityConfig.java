c8c3e567331cff3df0578f059ebdf684e740d562
heosangbeom <luv_3_u@naver.com>
2024-10-24 20:07:21 +0900

    fix(user): 시큐리티 filterChain 추가
    - 시큐리티 filterChain 추가

--- src/main/java/bgaebalja/bsherpa/configuration/SecurityConfig.java
+++ src/main/java/bgaebalja/bsherpa/configuration/SecurityConfig.java
@@ -28,10 +28,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -42,8 +43,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   private final JwtUtil jwtUtil;
   private final Environment env;
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.cors().and()
         .sessionManagement()
         .sessionCreationPolicy(SessionCreationPolicy.NEVER)
@@ -57,12 +58,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .addFilterBefore(new JwtCheckFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
         .exceptionHandling()
         .accessDeniedHandler(new CustomAccessDeniedHandler());
+
+    return http.build();
   }
 
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
+
+ /* @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .authorizeRequests(authorizeRequests ->
+            authorizeRequests.anyRequest().authenticated()
+        )
+        .formLogin(withDefaults());
+    return http.build();
+  }*/
 
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
