a6f9fa6c8e8608158daba9c5df5f47f4c68fdadf
heosangbeom <luv_3_u@naver.com>
2024-10-29 22:54:40 +0900

    refactor(user): 안 쓰는 프로퍼티 제거
    - 사용하지 않는 프로퍼티 제거

--- src/main/java/bgaebalja/bsherpa/validation/Valid.java
+++ src/main/java/bgaebalja/bsherpa/validation/Valid.java
@@ -4,9 +4,7 @@ import lombok.Getter;
 
 public enum Valid {
 
-  JOIN("join","회원가입 유효성 검증"),
-  SCENARIO("scenario","시나리오 등록, 수정, 유효성 검증");
-
+  JOIN("join", "회원가입 유효성 검증");
   @Getter
   private final String name;
   private final String description;
