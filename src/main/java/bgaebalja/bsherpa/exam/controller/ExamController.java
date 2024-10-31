8fd54ab9c6f8bb7fa44dcb30e5dca56bb276a8e6
React-test <gitdoolgi@gmail.com>
2024-10-31 17:14:21 +0900

    fix : ExamController 테스트 로그용 코드 삭제

--- src/main/java/bgaebalja/bsherpa/exam/controller/ExamController.java
+++ src/main/java/bgaebalja/bsherpa/exam/controller/ExamController.java
@@ -1,32 +1,27 @@
 package bgaebalja.bsherpa.exam.controller;
 
 import bgaebalja.bsherpa.exam.domain.RegisterExamRequest;
-import bgaebalja.bsherpa.exam.repository.ExamRepository;
 import bgaebalja.bsherpa.exam.service.ExamService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("exam")
 @RequiredArgsConstructor
 public class ExamController {
-    private static ExamService examService;
+    private final ExamService examService;
 
     @PostMapping("/step03/register")
     public ResponseEntity<Void> registerExam(
-            @RequestBody RegisterExamRequest registerExamRequest) {
-        System.out.println("getId : "+registerExamRequest.getEmail());
-        System.out.println("getName : "+registerExamRequest.getExamName());
-        System.out.println("getCollection : "+registerExamRequest.getCollections().get(0).getPassages().get(0));
+            @Valid @RequestBody RegisterExamRequest registerExamRequest) {
+        boolean successYn = examService.registerExam(registerExamRequest);
 
-        //boolean successYn = examService.registerExam(registerExamRequest);
-
-        //return successYn ? ResponseEntity.ok().build() : ResponseEntity.badRequest().build();
-        return ResponseEntity.ok().build();
+        return successYn ? ResponseEntity.ok().build() : ResponseEntity.badRequest().build();
     }
 }
