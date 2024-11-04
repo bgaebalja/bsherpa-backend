a2ea9f5f9dfe42a7f7221c1c7e67c81d07582f6f
React-test <gitdoolgi@gmail.com>
2024-11-04 16:17:47 +0900

    fix : RegisterCollectionRequest passage와 question에 collection 연관관계 주입 로직 추가, nopassage인 경우 로직 추가

--- src/main/java/bgaebalja/bsherpa/collection/domain/RegisterCollectionRequest.java
+++ src/main/java/bgaebalja/bsherpa/collection/domain/RegisterCollectionRequest.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Builder
@@ -13,10 +14,23 @@ public class RegisterCollectionRequest {
     private List<Passage> passages;
     private List<Question> questions;
 
-    public Collection toEntity(){
-        return Collection.builder()
-                .passages(passages)
+    public Collection toEntity() {
+        List<Passage> filteredPassages = passages.stream()
+                .filter(passage -> !("noPassage".equals(passage.getPassageId())))
+                .collect(Collectors.toList());
+
+
+        Collection collection = Collection.builder()
+                .passages(filteredPassages.isEmpty() ? null : filteredPassages)
                 .questions(questions)
                 .build();
+
+        filteredPassages.forEach(passage -> passage.assignCollection(collection));
+
+        if (questions != null) {
+            questions.forEach(question -> question.assignCollection(collection)); // Question에도 collection 할당
+        }
+
+        return collection;
     }
 }
