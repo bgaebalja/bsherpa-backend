0e14dbdc1b41c0dab56f47a2237016e8014175a7
Sung Hyobin <hellmir610@gmail.com>
2024-11-09 17:05:21 +0900

    fix(question): 같은 아이템 ID를 가지는 문제가 여러 개인 경우 여러 문제가 조회되는 문제 해결 (#160)
    - 단일 문제가 아닌 문제 목록을 조회하도록 수정

--- src/main/java/bgaebalja/bsherpa/question/repository/QuestionRepository.java
+++ src/main/java/bgaebalja/bsherpa/question/repository/QuestionRepository.java
@@ -3,8 +3,8 @@ package bgaebalja.bsherpa.question.repository;
 import bgaebalja.bsherpa.question.domain.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    Optional<Question> findByItemId(Long itemId);
+    List<Question> findByItemId(Long itemId);
 }
--- src/main/java/bgaebalja/bsherpa/question/service/QuestionServiceImpl.java
+++ src/main/java/bgaebalja/bsherpa/question/service/QuestionServiceImpl.java
@@ -2,15 +2,16 @@ package bgaebalja.bsherpa.question.service;
 
 import bgaebalja.bsherpa.question.domain.CreateQuestionRequest;
 import bgaebalja.bsherpa.question.domain.Question;
-import bgaebalja.bsherpa.question.exception.QuestionNotFoundException;
 import bgaebalja.bsherpa.question.repository.QuestionRepository;
+import bgaebalja.bsherpa.util.FormatValidator;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import static bgaebalja.bsherpa.question.exception.ExceptionMessage.QUESTION_NOT_FOUND_EXCEPTION_MESSAGE;
+import java.util.List;
+
 import static bgaebalja.bsherpa.util.LogConstant.QUESTION_NOT_SAVED_MESSAGE;
 import static org.springframework.transaction.annotation.Isolation.READ_COMMITTED;
 
@@ -30,17 +31,18 @@ public class QuestionServiceImpl implements QuestionService {
     @Override
     @Transactional(isolation = READ_COMMITTED, timeout = 10)
     public void addErrorReportCount(Long itemId) {
-        try {
-            Question question = getQuestion(itemId);
-            question.addErrorReport();
-            questionRepository.save(question);
-        } catch (QuestionNotFoundException qe) {
-            log.info(QUESTION_NOT_SAVED_MESSAGE);
+        List<Question> questions = getQuestions(itemId);
+        if (FormatValidator.hasValue(questions)) {
+            questions.stream().forEach(Question::addErrorReport);
+            questionRepository.saveAll(questions);
+
+            return;
         }
+
+        log.info(QUESTION_NOT_SAVED_MESSAGE);
     }
 
-    private Question getQuestion(Long itemId) {
-        return questionRepository.findByItemId(itemId)
-                .orElseThrow(() -> new QuestionNotFoundException(QUESTION_NOT_FOUND_EXCEPTION_MESSAGE));
+    private List<Question> getQuestions(Long itemId) {
+        return questionRepository.findByItemId(itemId);
     }
 }
